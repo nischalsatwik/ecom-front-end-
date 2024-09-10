f32eed8c338d7bfb8c8dbcd5f5a0249960f3371d
satwik <jhsasat@meta.com>
2024-09-10 06:47:57 -0700

    done with game.jsx

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,77 +1,60 @@
-import { useNavigate } from "react-router-dom";
-import {useRef} from "react";
-function Game() {
-    const p1Ref = useRef(null);
-  const p2Ref = useRef(null);
-  const p3Ref = useRef(null);
-  const navigate = useNavigate();
-  const inputRef = useRef(0);
-  const handleInputChange = (event) => {
-    inputRef.current.value = event.target.value;
-  };
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
-  let count = 0;
-  
-  let v3 = [];
+import {useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+export default function Game(){
+    const navigate=useNavigate();
+    const p1=["Your guess is right. The random number chosen is: ","you have guessed in ","your guess is high","your guess is low"];
+    const [p2,setP2]=useState(0);
+    const [p3,setP3]=useState([]);
+    const [Count,setCount]=useState(0);
+    const [randomNumber, setRandomNumber] = useState(null);
+    const [p5,setp5]=useState("");
+    const [disable,setDisable]=useState(false);
+    console.log("Random number=",randomNumber);
+  useEffect(() => {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }, []);
+    function calculate()
+    {
+        setCount(prev=>prev+1)
+        if(Count===5)
+        {
+             setp5(p1[1]);
+            setP3[null];
+            setP2(prev=>prev);
+            setDisable(true);
+        }
+        else if(p2==randomNumber)
+        {
+            console.log("equal");
+            setp5(p1[0]+" "+randomNumber);
+            setDisable(true);
 
-  function calculate() {    
-    if (count == 5) {
-      p2Ref.current.innerHTML = "You lost";
-      p3Ref.current.innerHTML = "Gameover";
-      count = 0;
-      v3 = [];
-    } else if (inputRef.current.value === randomNumber) {
-      p1Ref.current.innerHTML = "Your guess is right. The random number chosen is: " + randomNumber;
-      p2Ref.current.innerHTML = "you have guessed in " + v3.length + " chances";
-      p3Ref.current.innerHTML = "Gameover";
-      count = 0;
-      v3 = [];
-    } else if (inputRef.current.value > randomNumber) {
-      p1Ref.current.innerHTML = "your guess is high";
-      v3[count] = inputRef.current.value;
-      count = count + 1;
-      p2Ref.current.innerHTML = "your guesses: " + v3.join(", ");
-    } else {
-      p1Ref.current.innerHTML = "your guess is low";
-      v3[count] = inputRef.current.value;
-      count = count + 1;
-      p2Ref.current.innerHTML = "your guesses: " + v3.join(", ");
+        }
+        else if(p2>randomNumber)
+        {
+            console.log("more");
+            setp5(p1[2]);
+            setP3([...p3,p2]);
+            setP2(prev=>prev);
+        }
+        else{
+            setp5(p1[3]);
+            setP3([...p3,p2]);
+            setP2(prev=>prev);
+        }
     }
-
-    if (p3Ref.current.innerHTML === "Gameover") {
-        console.log("im inside")
-      return (
-        <div>
-          <button>
-            Yes
-          </button>
-          <button onClick={() => navigate("/next")}>
-            No
-          </button>
-        </div>
-      );
-    }
-  }
-
-  return (
-    <div className="mad">
-      <h1><b>Number guessing game</b></h1>
-      <pre>
-        <p id="p4">
-          We have selected a random number between 1 and 100. See if you can guess it in 6 turns or fewer.
-          <br />
-          We&apos;ll tell you if your guess was too high or too low.
-        </p>
-      </pre>
-      <div className="prompt1">
-        Enter the prompt: <input type="number" required min="1" max="100" id="input1"  ref={inputRef} onChange={handleInputChange}></input>
-        <button id="done" onClick={calculate}>submit</button>
-      </div>
-      <p ref={p1Ref}></p>
-      <p ref={p2Ref}></p>
-      <p ref={p3Ref}></p>
-    </div>
-  );
+    
+    return(<div>
+        <input type="text"  onChange={e=>setP2(e.target.value)} ></input>
+        <button onClick={calculate} disabled={disable? true : false}>Showup</button>
+        <p>{p5}</p>
+        <h1>{p3.join(",")}</h1>
+        {disable &&(
+            <>
+        <button onClick={()=>window.location.reload()}>Yes</button>
+        <button onClick={()=>navigate("/next")}>no</button>
+        </>)
 }
-
-export default Game;
+    </div>
+        );      
+}
